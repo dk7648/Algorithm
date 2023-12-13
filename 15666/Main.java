103eccaabbccae5f74946fc1694667b821f28753
dk7648 <dk7648@korea.ac.kr>
2023-12-13 21:30:49 +0900

    feat(BOJ): solve 15666

---
+++ 15666/Main.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    private static int[] result;
+    private static int[] arr;
+    private static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        result = new int[M];
+        arr = new int[N];
+
+        input = br.readLine().split(" ");
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        Arrays.sort(arr);
+
+        dfs(N,M,0, 0);
+        System.out.println(sb);
+    }
+    public static void dfs(int N, int M, int depth, int pos) {
+        if(depth == M) {
+            for(int num : result) {
+                sb.append(num).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        int before = -1;
+        for(int i=pos; i<N; i++) {
+            if(before == arr[i]) {
+                continue;
+            }
+            before = arr[i];
+            result[depth] = arr[i];
+            dfs(N,M,depth+1, i);
+        }
+    }
+}
